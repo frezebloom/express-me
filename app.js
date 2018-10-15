3efef2a49cd114da2f13ce76e19e3291bc6f3e3a
juxx <juxx@bk.ru>
2018-10-15 17:50:41 +0300

    refactoring

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const staticAsset = require("static-asset");
-
+const config = require("./config");
+const database = require("./database.js");
 const app = express();
+
+//database
+database()
+  .then(() => {
+    console.log(`Connected to database`);
+  })
+  .catch(error => {
+    console.log(`Unable to connect to database ${error}`);
+  });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +24,11 @@ app.use(
   express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
 );
 
+//route
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-module.exports = app;
+app.listen(config.PORT, () =>
+  console.log(`Start server on port ${config.PORT}`)
+);
