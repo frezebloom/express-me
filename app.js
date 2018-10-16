73954b35227e3b4646d5a0ae380f8ae0fca06ba7
juxx <juxx@bk.ru>
2018-10-16 10:55:54 +0300

    add .env and nodemon.json file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const staticAsset = require("static-asset");
 const config = require("./config");
 const database = require("./database.js");
+
+//express
 const app = express();
 
 //database
@@ -15,6 +17,7 @@ database()
     console.log(`Unable to connect to database ${error}`);
   });
 
+//sets and uses
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticAsset(path.join(__dirname, "public")));
@@ -24,9 +27,25 @@ app.use(
   express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
 );
 
-//route
+//routers
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render("error", {
+    message: error.message,
+    error: !config.IS_PRODUCTION ? error : {}
+  });
 });
 
 app.listen(config.PORT, () =>
