7b119d0811068f08e6eff6d8d137f441d4cd0375
juxx <juxx@bk.ru>
2018-10-19 09:26:25 +0300

    post

--- routes/archive.js
+++ routes/archive.js
@@ -15,25 +15,59 @@ function posts(req, res) {
     .skip(perPage * page - perPage)
     .limit(perPage)
     .then(posts => {
-      models.Post.countDocuments().then(count => {
-        res.render("index", {
-          posts,
-          current: page,
-          pages: Math.ceil(count / perPage),
-          user: {
-            id: userId,
-            login: userLogin
-          }
+      models.Post.countDocuments()
+        .then(count => {
+          res.render("index", {
+            posts,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            user: {
+              id: userId,
+              login: userLogin
+            }
+          });
+        })
+        .catch(() => {
+          throw new Error("Server Error");
         });
-      });
     })
-    .catch(error => {
-      console.log(error);
+    .catch(() => {
+      throw new Error("Server Error");
     });
 }
 
 router.get("/", (req, res) => posts(req, res));
 
 router.get("/archive/:page", (req, res) => posts(req, res));
+
+router.get("/posts/:post", (req, res, next) => {
+  const url = req.params.post.trim().replace(/ +(?= )/g, "");
+  const userId = req.session.userId;
+  const userLogin = req.session.userLogin;
+
+  if (!url) {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    models.Post.findOne({
+      url
+    }).then(post => {
+      if (!post) {
+        const err = new Error("Not Found");
+        err.status = 404;
+        next(err);
+      } else {
+        res.render("post/post", {
+          post,
+          user: {
+            id: userId,
+            login: userLogin
+          }
+        });
+      }
+    });
+  }
+});
 
 module.exports = router;
