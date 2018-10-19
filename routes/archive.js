41e2e84a8fd0f444c98f187217bc831b1a59818d
juxx <juxx@bk.ru>
2018-10-19 10:57:58 +0300

    user

--- routes/archive.js
+++ routes/archive.js
@@ -7,17 +7,18 @@ const models = require("../models");
 function posts(req, res) {
   const userId = req.session.userId;
   const userLogin = req.session.userLogin;
-
   const perPage = +config.PER_PAGE;
   const page = req.params.page || 1;
 
   models.Post.find({})
     .skip(perPage * page - perPage)
     .limit(perPage)
+    .populate("owner")
+    .sort({ createdAt: -1 })
     .then(posts => {
       models.Post.countDocuments()
         .then(count => {
-          res.render("index", {
+          res.render("archive/index", {
             posts,
             current: page,
             pages: Math.ceil(count / perPage),
@@ -36,8 +37,8 @@ function posts(req, res) {
     });
 }
 
+// routers
 router.get("/", (req, res) => posts(req, res));
-
 router.get("/archive/:page", (req, res) => posts(req, res));
 
 router.get("/posts/:post", (req, res, next) => {
@@ -68,6 +69,49 @@ router.get("/posts/:post", (req, res, next) => {
       }
     });
   }
+});
+
+// users posts
+router.get("/users/:login/:page*?", (req, res) => {
+  const userId = req.session.userId;
+  const userLogin = req.session.userLogin;
+  const perPage = +config.PER_PAGE;
+  const page = req.params.page || 1;
+  const login = req.params.login;
+
+  models.User.findOne({
+    login
+  }).then(user => {
+    models.Post.find({
+      owner: user.id
+    })
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 })
+      .then(posts => {
+        models.Post.countDocuments({
+          owner: user.id
+        })
+          .then(count => {
+            res.render("archive/user", {
+              posts,
+              _user: user,
+              current: page,
+              pages: Math.ceil(count / perPage),
+              user: {
+                id: userId,
+                login: userLogin
+              }
+            });
+          })
+          .catch(() => {
+            throw new Error("Server Error");
+          });
+      })
+      .catch(() => {
+        throw new Error("Server Error");
+      });
+  });
 });
 
 module.exports = router;
